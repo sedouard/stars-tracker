22318beb9cd18859104335a2badf5e24cde75071
Steven Edouard <sedouard@microsoft.com>
2015-12-28 23:55:19 -0500

    fix(modules/mongo_helper) - reuse mongo connection

--- modules/mongo_helper.js
+++ modules/mongo_helper.js
@@ -1,15 +1,29 @@
 var MongoClient = require('mongodb').MongoClient,
-  conf = require('./config');
+    conf = require('./config'),
+    connection = null,
+    debug = require('debug')('stars-tracker:mongodb');
 
 var connect = function () {
   // Connection URL
   var url = conf.get('MONGO_URL');
 
+  if (connection) {
+    return new Promise((resolve, reject) => {
+      debug('reusing mongodb connection');
+      return resolve(connection);
+    });;
+  }
+
   return MongoClient.connect(url)
-    .catch(err => {
-      console.error('Failed to conenct to: ' + url);
-      console.error(err);
-    });
+  .then(conn => {
+    debug('successfully connected to mongodb');
+    connection = conn;
+    return connection;
+  })
+  .catch(err => {
+    console.error('Failed to conenct to: ' + url);
+    console.error(err);
+  });
 };
 
 module.exports = {
